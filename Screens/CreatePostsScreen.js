c51dadd485bb24ef49f29f44dfa34c95ee5dbd9a
KhailoAndrey <hap512@gmail.com>
2023-05-02 17:04:56 +0300

    navi

--- Screens/CreatePostsScreen.js
+++ Screens/CreatePostsScreen.js
@@ -1,7 +1,18 @@
+import { StyleSheet, Text, View } from "react-native"
 
 export default function CreatePostsScreen() { 
 
     return (
-        <View></View>
+        <View style={styles.container}>
+            <Text>CreatePostsScreen</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,7 +1,17 @@
-
+import { StyleSheet, Text, View } from "react-native"
 export default function ProfileScreen() { 
 
     return (
-        <View></View>
+        <View style={styles.container}>
+            <Text>ProfileScreen</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
