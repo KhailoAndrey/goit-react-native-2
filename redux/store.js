1602eae5bb52de8434ab08e9ff1a5b8fb8e2c74d
KhailoAndrey <hap512@gmail.com>
2023-08-28 19:42:21 +0300

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import { authSlice } from './auth/authReducer';
+import authReducer from './auth/authReducer';
 
 const rootReducer = combineReducers({
-    [authSlice.name]: authSlice.reducer,
-
+    auth: authReducer
 })
 
 export const store = configureStore({
